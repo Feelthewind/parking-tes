400dcf3e196eb22636c55870404c5d3e9c173487
lcs <skyisthelimitless@gmail.com>
2019-09-07 17:19:51 +0900

    auth

---
+++ nodejs/src/auth/dto/signup.dto.ts
@@ -0,0 +1,40 @@
+import {
+  IsBoolean,
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
+export class SignUpDTO {
+  @MaxLength(100)
+  @IsNotEmpty()
+  name: string;
+
+  @IsString()
+  @MinLength(4)
+  @MaxLength(100)
+  @IsNotEmpty()
+  password: string;
+
+  @IsString()
+  @IsEmail()
+  @MinLength(4)
+  @MaxLength(100)
+  @IsNotEmpty()
+  email: string;
+
+  @IsString()
+  @MinLength(4)
+  @MaxLength(100)
+  @IsNotEmpty()
+  address: string;
+
+  @IsBoolean()
+  @IsNotEmpty()
+  is_disabled: boolean;
+
+  @IsNotEmpty()
+  type: string;
+}
